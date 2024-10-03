cd836a14e70e0a81bfb69dbefec2907d48ab2b6e
Kirill Timofejev <54275259+jawur@users.noreply.github.com>
2024-10-03 13:02:31 +0100

    Tidy up

--- src/Providers/LaravelDtoServiceProvider.php
+++ src/Providers/LaravelDtoServiceProvider.php
@@ -6,13 +6,5 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelDtoServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        // Register bindings, listeners, etc.
-    }
-
-    public function boot()
-    {
-        // Publish configurations, migrations, etc.
-    }
+    //
 }
